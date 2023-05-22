db759ea2a50b579fa257b18083e9473af962f34b
deu <deu@DESKTOP-DGUV990>
2023-05-22 09:55:01 +0900

    가상함수 소멸자, 오버라이딩

---
+++ chapter09/t_1.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//
+//class Parent {
+//public:
+//	virtual void draw() final {
+//		cout << "1";
+//	}
+//};
+//
+//class Child : public Parent {
+//	virtual void draw() {
+//		cout << "2";
+//	}
+//};
---
+++ chapter09/t_2.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//class Parent {
+//public:
+//	virtual ~Parent() { cout << "ºÎ¸ð »ç¸Á" << endl << endl; }
+//};
+//
+//class Child : public Parent {
+//public:
+//	virtual ~Child() { cout << "ÀÚ½Ä »ç¸Á" << endl; }
+//};
+//
+//int main() {
+//	Child* c = new Child();
+//	Parent* pc = new Child();
+//
+//	delete c;
+//	delete pc;
+//}
